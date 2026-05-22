608708a70979a1efccac9762573311dfd2eae0c1
Esha Maryam <eshamaryam1076@gmail.com>
2026-05-22 07:55:44 +0500

    Updated server port

--- server.js
+++ server.js
@@ -314,6 +314,8 @@ app.put("/update-booking-status/:id", (req, res) => {
 
 
 // ===== START SERVER =====
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
